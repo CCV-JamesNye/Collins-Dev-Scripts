c4b1777a639d4f915e9aa1b9520779512953e57d
CollinStoodley <260505701+CollinStoodley@users.noreply.github.com>
2026-02-24 18:12:54 -0600

    Commit for checking work

---
+++ Week 4 Physics Work/physics-work/player_1.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+var speed : float = 400
+
+
+
+# Apply Movement
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	# Store Direction
+	var direction : Vector2 = Vector2.ZERO
+	
+	# Read input
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_down"):
+		direction.y += 1
+	if Input.is_action_pressed("move_up"):
+		direction.y -= 1
+	
+		velocity += direction.normalized() * speed
+		move_and_slide()
